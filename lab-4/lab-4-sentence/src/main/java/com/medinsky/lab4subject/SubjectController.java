6ea47e532b8de32c58e8898520ca5cec72d3795a
Medinsky Alex <alexandr@shieldfc.com>
2019-08-22 18:01:14 +0300

    lab 4 eureka client sentence

---
+++ lab-4/lab-4-sentence/src/main/java/com/medinsky/lab4subject/SubjectController.java
@@ -0,0 +1,44 @@
+package com.medinsky.lab4subject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class SubjectController {
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @GetMapping("/sentence")
+    public @ResponseBody
+    String getWord() {
+        return getWord("LAB-4-SUBJECT") + " "
+                + getWord("LAB-4-VERB") + " "
+                + getWord("LAB-4-ARTICLE") + " "
+                + getWord("LAB-4-ADJECTIVE") + " "
+                + getWord("LAB-4-NOUN") + "."
+                ;
+    }
+
+    public String getWord(String service) {
+        List<ServiceInstance> instances = client.getInstances(service);
+        if (instances != null && instances.size() > 0) {
+            URI uri = instances.get(0).getUri();
+            if (uri != null) {
+                return (new RestTemplate()).getForObject(uri, String.class);
+            }
+        }
+        return null;
+    }
+
+
+}
